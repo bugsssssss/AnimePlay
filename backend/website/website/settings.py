e1cea17f069b202fe850978fe359f08e2fb51d1d
bugsssssss <n.soatov@bk.ru>
2023-06-07 06:36:07 +0000

    prod commit #1

--- backend/website/website/settings.py
+++ backend/website/website/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-w44jse^d-66r78-pv6o^w%(0#$0&p5!0zn=qr(r9^c)71ocgrz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['45.12.238.200']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
@@ -114,9 +114,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = 'api/static/'
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
